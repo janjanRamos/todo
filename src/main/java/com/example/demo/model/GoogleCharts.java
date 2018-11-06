f5ffa3460a2edde064ddd9583178fdfeb2b0544f
janjan_ramos <janjan_ramos@hotmail.com>
2018-11-06 16:47:03 -0300

    #Jair Leite

---
+++ src/main/java/com/example/demo/model/GoogleCharts.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+public class GoogleCharts {
+
+	private int done;
+	private int undone;
+
+	public int getDone() {
+		return done;
+	}
+
+	public void setDone(int done) {
+		this.done = done;
+	}
+
+	public int getUndone() {
+		return undone;
+	}
+
+	public void setUndone(int undone) {
+		this.undone = undone;
+	}
+
+}
